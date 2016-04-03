c3d3d0b1f18840315ce1a996743ee3ad66e78f3f
Georgios Papoutsakis <g.papoutsakis86@gmail.com>
2016-04-03 03:07:17 +0200

    Exercise B

--- scater.js
+++ scater.js
@@ -1,33 +1,144 @@
 app = angular.module('app');
 
 app.controller("scaterController", ["$scope", function($scope){
-    function load_scater(file_name) {
+    console.log('skata');
+    var totalprostitutions, totalvehicles;
+    var margin = {top: 20, right: 100, bottom: 30, left: 80}
+    var width = 1000 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
+
+    var xValue = function(d) { return d.prostitutions;}, // data -> value
+    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xMap = function(d) {
+        return xScale(xValue(d));
+    }, // data -> display
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+    var yValue = function(d) {return d.vehiclethefts2003;}, // data -> value
+    yScale = d3.scale.linear().range([height, 0]), // value -> display
+    yMap = function(d) {return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+    var cValue = function(d) { return d.district2003;},
+    color = d3.scale.category10();
+
+    var radius = function(d) {
+        return d.crimes2003 * 30 /maxcrimes;
+    };
+
+    var crimes = function(d){
+        return d.crimes2003;
+    }
+    var maxcrimes;
+
+
+
+    function initialize(file_name) {
         d3.csv(file_name, function (dataset) {
             dataset.forEach(function (d) {
-                d.crimes = parseInt(d.crimes);
+                d.crimes2003 = +d.crimes2003;
+                d.vehiclethefts2003 = +d.vehiclethefts2003;
+                d.prostitutions2003 = +d.prostitutions2003;
 
             });
+            var svg = d3.select("#scater").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var tooltip = d3.select("#scater").append("div")
+              .attr("class", "tooltip")
+              .style("opacity", 0);
+
+
+            xScale.domain([-300, d3.max(dataset, xValue)]);
+            yScale.domain([-300, d3.max(dataset, yValue)]);
+
+            maxcrimes = d3.max(dataset, crimes);
+            console.log(maxcrimes);
+            // x-axis
+            svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis)
+              .append("text")
+              .attr("class", "label")
+              .attr("x", width)
+              .attr("y", -6)
+              .style("text-anchor", "end")
+              .text("prostitutions");
+            // y-axis
+            svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+              .append("text")
+              .attr("class", "label")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("vehicle thefts");
+
+            svg.selectAll(".dot")
+              .data(dataset)
+              .enter().append("circle")
+              .attr("class", "dot")
+              .attr("r", radius)
+              .attr("cx", xMap)
+              .attr("cy", yMap)
+              .style("fill", function(d) { return color(cValue(d));})
+              .on("mouseover", function(d) {
+                tooltip.transition()
+                  .duration(200)
+                  .style("opacity", .9);
+                tooltip.html(d.district + "<br/> prostitutions:" + xValue(d)
+                  + "<br/> vehicle thefts:" + yValue(d) + "")
+                  .style("left", (d3.event.pageX - 200) + "px")
+                  .style("top", (d3.event.pageY - 128) + "px");
+            })
+            .on("mouseout", function(d) {
+              tooltip.transition()
+                   .duration(500)
+                   .style("opacity", 0);
+            });
+
+            // draw legend
+            var legend = svg.selectAll(".legend")
+              .data(color.domain())
+              .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(100," + (150 + i * 20) + ")"; });
+
+            // draw legend colored rectangles
+            legend.append("rect")
+              .attr("x", width - 18)
+              .attr("width", 18)
+              .attr("height", 18)
+              .style("fill", color);
+
+            // draw legend text
+            legend.append("text")
+              .attr("x", width - 24)
+              .attr("y", 9)
+              .attr("dy", ".35em")
+              .style("text-anchor", "end")
+              .text(function(d) { return d;});
         });
     }
-    load_scater('scater_plot/scater_plot_data.csv');
+    initialize('http://giorgospa.github.io/scater_plot/scater_plot_data.csv');
 }]);
 
-// var margin = {top: 20, right: 20, bottom: 30, left: 40},
-//     width = 960 - margin.left - margin.right,
-//     height = 500 - margin.top - margin.bottom;
 
-// /*
-//  * value accessor - returns the value to encode for a given data object.
-//  * scale - maps value to a visual display encoding, such as a pixel position.
-//  * map function - maps from data value to display value
-//  * axis - sets up axis
-//  */
 
-// // setup x
-// var xValue = function(d) { return d.Calories;}, // data -> value
-//     xScale = d3.scale.linear().range([0, width]), // value -> display
-//     xMap = function(d) { return xScale(xValue(d));}, // data -> display
-//     xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+/*
+ * value accessor - returns the value to encode for a given data object.
+ * scale - maps value to a visual display encoding, such as a pixel position.
+ * map function - maps from data value to display value
+ * axis - sets up axis
+ */
+
+// setup x
 
 // // setup y
 // var yValue = function(d) { return d["Protein (g)"];}, // data -> value
@@ -116,7 +227,7 @@ app.controller("scaterController", ["$scope", function($scope){
 //   // draw legend
 //   var legend = svg.selectAll(".legend")
 //       .data(color.domain())
-//     .enter().append("g")
+//       .enter().append("g")
 //       .attr("class", "legend")
 //       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
@@ -134,4 +245,4 @@ app.controller("scaterController", ["$scope", function($scope){
 //       .attr("dy", ".35em")
 //       .style("text-anchor", "end")
 //       .text(function(d) { return d;})
-// });
+//});
